7c1ea4679f3a832ab9f31635f715c7b7a2e694f3
Carson <c@rsonriker.com>
2019-10-29 17:34:39 -0400

    Created componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FullListComponent } from './full-list/full-list.component';
+import { ShortListComponent } from './short-list/short-list.component';
+import { RecommendComponent } from './recommend/recommend.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullListComponent,
+    ShortListComponent,
+    RecommendComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
