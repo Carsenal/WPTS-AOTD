b126a720471953cd91cda3b13f9e683aad4f44ea
Carson <c@rsonriker.com>
2019-10-29 17:50:01 -0400

    Configured routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { FullListComponent }  from './full-list/full-list.component';
+import { ShortListComponent } from './short-list/short-list.component';
+import { QuizComponent }      from './quiz/quiz.component';
+import { RecommendComponent } from './recommend/recommend.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'fullList',   component: FullListComponent },
+  { path: 'shortList',  component: ShortListComponent },
+  { path: 'quiz',       component: QuizComponent },
+  { path: 'recommend',  component: RecommendComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
